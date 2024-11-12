7e0e8da42a0fb827af0eea9b6a424838dff599eb
dev4anand <iamanand1702@gmail.com>
2024-11-12 21:16:06 +0530

    added js to the page

---
+++ main.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $('#exampleModal').modal('show');
+
+    // Hide the modal after 5 seconds (5000 milliseconds)
+    setTimeout(function() {
+      $('#exampleModal').modal('hide');
+    }, 2000);
+  });
