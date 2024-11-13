305f3e254d2b65a5872024ef6c8bc42d63718476
dev4anand <iamanand1702@gmail.com>
2024-11-13 06:17:43 +0530

    some changes

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     $('#exampleModal').modal('show');
 
     // Hide the modal after 5 seconds (5000 milliseconds)
-    setTimeout(function() {
+    // setTimeout(function() {
       $('#exampleModal').modal('hide');
-    }, 2000);
+    // }, 2000);
   });
